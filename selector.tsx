70007ede6e4f182401cd993851f3113b7bb6bbee
Awais Ahmad <58293801+awaisahmadfg@users.noreply.github.com>
2026-01-04 20:55:22 +0500

    Delete selector.tsx

--- selector.tsx
+++
@@ -1,6 +0,0 @@
-export const GetLocationTagsLoading = () =>
-  useSelector((state: any) => state.home.locationTagsLoading);
-export const GetWorkplaceTagsLoading = () =>
-  useSelector((state: any) => state.home.workplaceTagsLoading);
-export const GetUniversityTagsLoading = () =>
-  useSelector((state: any) => state.home.universityTagsLoading);
